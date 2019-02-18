6898830215813902c052382deeac66358caed1d7
Andrew Elliott <43253284+Critchkn@users.noreply.github.com>
2019-02-18 09:49:15 -0600

    Working on Calculator app

--- calculator.js
+++ calculator.js
@@ -1,42 +1,59 @@
-var a = [];
-var b = [];
-var c = [];
-var operator = [];
-var input = [];
+//let numField1 = document.getElementById('numField1');
+//let numField2 = document.getElementById('numField2');
+//let resultField = document.getElementById('resultField');
+//let form = document.getElementById('xIsWhatPercentOfY');
+//
+//form.addEventListener('submit', function(event){
+//    if(!numField1.value || !numField2.value){
+//        alert('Please Enter a Value');
+//    } else {
+//        var x = parseFloat(numField1.value);
+//        var y = parseFloat(numField2.value);
+//        var result = x / y;
+//        var percent = result * 100;
+//        resultField.innerText = "Answer: " + percent + "%";
+//        event.preventDefault();
+//    }
+//})
 
-function calculatorOperations(a, b, operator){
-  if(typeof a !== 'number' || typeof b !== "number"){
-         return 'error, must enter a number';
-        } 
-       switch(operator){
-        case '+':
-        return a + b;
-        case '-':
-        return a - b;
-        case '*':
-        return a * b;
-        case '/':
-        return a / b;
-  }
+
+
+var oneBtn = document.getElementById('calc-one');
+var twoBtn = document.getElementById('calc-two');
+var threeBtn = document.getElementById('calc-three');
+var fourBtn = document.getElementById('calc-four');
+var fiveBtn = document.getElementById('calc-five');
+var sixBtn = document.getElementById('calc-six');
+var sevenBtn = document.getElementById('calc-seven');
+var eightBtn = document.getElementById('calc-eight');
+var nineBtn = document.getElementById('calc-nine');
+var zeroBtn = document.getElementById('calc-zero');
+
+var decimalBtn = document.getElementById('calc-decimal');
+var clrBtn = document.getElementById('calc-clear');
+var bkspcBtn = document.getElementById('calc-backspace');
+var displayvalElement = document.getElementById('calc-display-val');
+
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
+var calcNumbtns = document.getElementsByClassName('calc-btn-num');
+var calcOperatorBtns = document.getElementsByClassName('calc-operator');
+
+var updateDisplayVal = function (onclick) {
+    var btnText = onclick.target.innerHTML;
+    if (displayVal === '0') {
+        displayVal = "";
+        displayVal += btnText;
+        displayvalElement.innerHTML = displayVal;
+    }
+};
+
+for (let i = 0; i < calcNumbtns.length; i++) {
+    calcNumbtns[i].addEventListener('click' updateDisplayVal, false);
 }
 
-let numField1 = document.getElementById('numField1');
-let numField2 = document.getElementById('numField2');
-let resultField = document.getElementById('resultField');
-let form = document.getElementById('xIsWhatPercentOfY');
-
-form.addEventListener('submit', function(event){
-    if(!numField1.value || !numField2.value){
-        alert('Please Enter a Value');
-    } else {
-        var x = parseFloat(numField1.value);
-        var y = parseFloat(numField2.value);
-        var result = x / y;
-        var percent = result * 100;
-        resultField.innerText = "Answer: " + percent + "%";
-        event.preventDefault();
-    }
-})
-
-
-
+//for (let i = 0; i < calcOperatorBtns.length; i++) {
+//  calcOperatorBtns[i],addEventListener('click', performOperation, false);  
+//}
